b8e2a88d9f54fea3e9dcff92db09a3575a2bbda5
Nathan Green <nathan.green@vibriant.net>
2014-03-12 21:37:38 -0500

    Added host, template, description, nopassword to createdb. Added failOnError option to cause a build failure if createdb or initdb fail

--- src/main/java/com/github/adrianboimvaser/postresql/plugin/CreatedbMojo.java
+++ src/main/java/com/github/adrianboimvaser/postresql/plugin/CreatedbMojo.java
@@ -19,7 +19,20 @@ public class CreatedbMojo extends PgsqlMojo {
     protected String databaseName;
 
     @Parameter
+    protected String description;
+
+    @Parameter
+    protected String host;
+
+    @Parameter
     protected Integer port;
+
+    @Parameter
+    protected String template;
+
+    /** If specified, password prompts are disabled (may result in an error) */
+    @Parameter(alias = "no-password", defaultValue = "false", property = "no-password")
+    protected String noPassword;
 
     public void execute() throws MojoExecutionException, MojoFailureException {
         if (skip) {
@@ -33,21 +46,33 @@ public class CreatedbMojo extends PgsqlMojo {
         }
 
         final ProcessBuilder processBuilder = new ProcessBuilder(cmd);
+        String message = "";
+        Exception cause = null;
+        int returnValue = Integer.MIN_VALUE;
         try {
             Process process = processBuilder.start();
             logOutput(process);
-            int returnValue = process.waitFor();
-            getLog().debug("createdb returned " + returnValue);
-        } catch (IOException e) {
+            returnValue = process.waitFor();
+            message = "createdb returned " + returnValue;
+            getLog().debug(message);
+        } catch (IOException|InterruptedException e) {
+            message = e.getLocalizedMessage();
+            cause = e;
             getLog().error(e);
-        } catch (InterruptedException e) {
-            getLog().error(e);
+        }
+        if (returnValue != 0 && failOnError) {
+            throw new MojoExecutionException(message, cause);
         }
     }
 
     private List<String> createCommand() throws MojoExecutionException {
         List<String> cmd = new ArrayList<String>();
         cmd.add(getCommandPath("createdb"));
+
+        if (host != null) {
+            cmd.add("-h");
+            cmd.add(host);
+        }
 
         if (port != null) {
             cmd.add("-p");
@@ -59,7 +84,23 @@ public class CreatedbMojo extends PgsqlMojo {
             cmd.add(username);
         }
 
+        if (template != null) {
+            cmd.add("-T");
+            cmd.add(template);
+        }
+
+        if (noPassword != null) {
+            // accept zero-or-more whitespace, or `true`
+            noPassword = noPassword.trim().toLowerCase();
+            if ("".equals(noPassword) || "true".equals(noPassword)) {
+                cmd.add("--no-password");
+            }
+        }
+
         cmd.add(databaseName);
+        if (description != null) {
+            cmd.add(description);
+        }
 
         return cmd;
     }
