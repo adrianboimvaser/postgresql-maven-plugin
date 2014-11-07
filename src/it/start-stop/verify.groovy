3524aeab8e3b91413332e452390bd8386d88be95
Adrián Boimvaser <adrianboimvaser@gmail.com>
2014-11-07 14:03:36 -0300

    Deleted context-sensitive test.

--- src/it/start-stop/verify.groovy
+++ src/it/start-stop/verify.groovy
@@ -1,10 +1,2 @@
-final touchFile = new File(basedir, "target/postgres.log");
-assert touchFile.isFile()
-final csvFile = new File(basedir, "target/sql-output.csv");
-assert csvFile.isFile()
-final csv = csvFile.text
-assert '''encoding,pg_encoding_to_char,datcollate,datdba
-6,UTF8,sv_SE,10
-
-0 rows affected
-'''.equals(csv)
+File logFile = new File(basedir, "target/postgres.log");
+assert logFile.isFile()
